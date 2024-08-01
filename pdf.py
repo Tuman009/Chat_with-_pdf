c1624f3b36e3198f7a8153e51052ea36b0232b77
Tuman Tufail v <168721450+Tuman009@users.noreply.github.com>
2024-08-01 15:07:51 +0530

    Update pdf.py

--- pdf.py
+++ pdf.py
@@ -3,7 +3,6 @@ import streamlit as st
 from dotenv import load_dotenv
 import google.generativeai as gen_ai
 import fitz  # PyMuPDF library
-import io  # To handle file-like objects
 
 # Load environment variables
 load_dotenv()
@@ -22,11 +21,13 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 if not GOOGLE_API_KEY:
     st.error("Google API key is not set. Please check your environment variables.")
 else:
-    # Set up Google Gemini-Pro AI model
-    gen_ai.configure(api_key=GOOGLE_API_KEY)
-    model = gen_ai.GenerativeModel('gemini-pro')
+    # Set up Google Gemini AI
+    try:
+        gen_ai.configure(api_key=GOOGLE_API_KEY)
+    except Exception as e:
+        st.error(f"Error configuring Google AI model: {e}")
 
-# Function to translate roles between Gemini-Pro and Streamlit terminology
+# Function to translate roles between Gemini AI and Streamlit terminology
 def translate_role_for_streamlit(user_role):
     if user_role == "model":
         return "assistant"
@@ -50,7 +51,10 @@ def extract_text_from_pdf(pdf_file):
 # Initialize chat session in Streamlit if not already present
 if "chat_session" not in st.session_state:
     try:
-        st.session_state.chat_session = model.start_chat(history=[])
+        st.session_state.chat_session = gen_ai.chat.start_chat(
+            model="models/chat-bison-001",
+            history=[],
+        )
     except Exception as e:
         st.error(f"Failed to start chat session: {e}")
 
@@ -68,9 +72,9 @@ if uploaded_file:
 
 # Display the chat history
 if "chat_session" in st.session_state:
-    for message in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
-            st.markdown(message.parts[0].text)
+    for message in st.session_state.chat_session['history']:
+        with st.chat_message(translate_role_for_streamlit(message['author'])):
+            st.markdown(message['content'])
 
 # Input field for user's message
 user_prompt = st.chat_input("Ask CHAT WITH PDF...")
@@ -88,11 +92,11 @@ if user_prompt:
             combined_prompt = f"Question: {user_prompt}"
 
         try:
-            # Send user's message and PDF context to Gemini-Pro and get the response
+            # Send user's message and PDF context to Gemini AI and get the response
             gemini_response = st.session_state.chat_session.send_message(combined_prompt)
 
-            # Display Gemini-Pro's response
+            # Display Gemini AI's response
             with st.chat_message("assistant"):
-                st.markdown(gemini_response.text)
+                st.markdown(gemini_response['candidates'][0]['content'])
         except Exception as e:
             st.error(f"Error communicating with the AI model: {e}")
